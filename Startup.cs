e7618e4c4bd8eb2ef90f09132f8d469c5fbaa962
Михаил Проказин <bearpro@outlook.com>
2020-06-15 20:56:50 +0300

    Добавлен CORS.

--- Startup.cs
+++ Startup.cs
@@ -46,16 +46,14 @@ namespace team_test
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            //app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseCors(config => config
+                .AllowAnyOrigin()
+                .AllowAnyHeader());
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
